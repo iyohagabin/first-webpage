e779b68f4a4595b8337bf65f23baf0166332f5de
iyoha gabin <gabin4star@gmail.com>
2020-03-25 16:36:42 +0100

    java script task

---
+++ java script task.js
@@ -0,0 +1,7 @@
+var name = "iyoha ijebor gabin"
+var course = ["kotlin","javascript","html","css","java","figma","flutter"]
+console.log(name)
+console.log(course)
+for(i=1; i<=200; i++){
+    if(i%2==0){console.log(i)}}
+
